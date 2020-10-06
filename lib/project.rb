6c24321f07a262353ba4172e8ab1e56dddc47770
Michelle Ranly <meranly8@gmail.com>
2020-10-06 02:32:30 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project
   attr_reader :title 
-  attr_accessor :backed_projects
+  attr_accessor :backers
   
   def initialize(title)
     @title = title
-    @backed_projects = []
+    @backers = []
   end
 end
